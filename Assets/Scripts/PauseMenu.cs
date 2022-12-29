adb3b3ec943a7189529c8ed96fb4ee464f76063a
SouthBarbol <73181761+SouthBarbol@users.noreply.github.com>
2022-12-29 12:59:47 +0100

    MenuDePause_v1

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseButton;
+
+    [SerializeField] private GameObject pauseMenu;
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void Replay()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Ta luego");
+        Application.Quit();
+    }
+}
